2de562b41c1d2f3025b27b7972ebb64a0be19f48
Connor Murphy <connor.murphy@bell.net>
2016-06-04 16:26:17 -0400

    Added code to ensure unix systems use open gl when running application from jar

--- main/Main.java
+++ main/Main.java
@@ -2,29 +2,38 @@ package main;
 
 import gui.GameFrame;
 
-public class Main
-{
-	private static GameFrame frame;
-	public static void main(String[] args)
-	{
-		// Ensure not using command line args
-		if (args.length > 0)
-		{
-			System.err.println("USAGE: no command line arguments");
-			System.exit(0);
-		}
-		
-		//Start the program
-		frame = new GameFrame();
-	}
-	
-	public static int getHeight()
-	{
-		return frame.getHeight();
-	}
-	
-	public static int getWidth()
-	{
-		return frame.getWidth();
-	}
+import java.io.IOException;
+
+public class Main {
+    private static GameFrame frame;
+
+    public static void main(String[] args) {
+        //Set command line args if not already using
+
+        //TODO: uncomment when have a jar file
+        /*
+        if (args.length == 0) {
+            try {
+                //Find if running a unix system
+                String os = System.getProperty("os.name").toLowerCase();
+                if (os.contains("nix") || os.contains("nux") || os.contains("aix")) {
+                    // Relaunch the application using opengl to render for performance reasons
+                    Runtime.getRuntime().exec(new String[]{"java", "-Dsun.java2d.opengl=True", "-jar", "BindingOfMangat.jar"});
+                }
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+        } else {*/
+            //Start the program
+            frame = new GameFrame();
+        //}
+    }
+
+    public static int getHeight() {
+        return frame.getHeight();
+    }
+
+    public static int getWidth() {
+        return frame.getWidth();
+    }
 }
