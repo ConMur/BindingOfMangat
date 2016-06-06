472ae0af6518d5f018d14e0aa43175ac1bfd52e5
sunmatthew <matthewsun3@gmail.com>
2016-06-05 22:41:26 -0400

    room bounds

--- thingsthatmove/MoveableObject.java
+++ thingsthatmove/MoveableObject.java
@@ -7,10 +7,10 @@ import java.awt.Image;
 
 public class MoveableObject extends GameObject
 {
-	private final int LOWER_X_BOUND = 0;
-	private final int UPPER_X_BOUND = 720;
-	private final int LOWER_Y_BOUND = 0;
-	private final int UPPER_Y_BOUND = 1280;
+	private final int LOWER_X_BOUND = 90;
+	private final int UPPER_X_BOUND = 960;
+	private final int LOWER_Y_BOUND = 200;
+	private final int UPPER_Y_BOUND = 672;
 	private final double ROOT_TWO = Math.sqrt(2);
 	private int dmg, hp, speed, maxHP;
 	
