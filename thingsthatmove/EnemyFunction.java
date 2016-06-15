aa3ed822a77e93a37b629637fb169c0ef01f05c0
Connor Murphy <connor.murphy@bell.net>
2016-06-15 11:53:00 -0400

    ok

--- thingsthatmove/EnemyFunction.java
+++ thingsthatmove/EnemyFunction.java
@@ -12,6 +12,6 @@ public class EnemyFunction extends Enemy
 	public EnemyFunction () throws IOException
 	{
 		super (new Enemy(1, 30, 200, 400, 400, ImageIO.read(LevelManager.class.getResourceAsStream("/images/enemies/functionfront.png")),
-                new Dimension(51, 25), 30, true, true, new Dimension(44, 9), 6, 43));
+                new Dimension(75, 37), 30, true, true, new Dimension(62, 14), 9, 64));
 	}
 }
