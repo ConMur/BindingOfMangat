993bba82628aa4bd43a6e248c4c69cb1a3c64202
Connor Murphy <connor.murphy@bell.net>
2016-06-15 11:50:31 -0400

    ridout cutscenes

--- thingsthatmove/Ridout.java
+++ thingsthatmove/Ridout.java
@@ -1,7 +1,9 @@
 package thingsthatmove;
 
-import java.awt.Dimension;
-import java.awt.Image;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 /**
  * The final boss of the game. Ridout has two forms, an initial easy from and a secondary harder form.
@@ -10,12 +12,36 @@ public class Ridout extends Enemy
 {
 	private int numLives;
 	private boolean upgraded;
+
+	private boolean playCutScene, playedFirstCutscene;
+	private boolean doneCutscene;
+	private BufferedImage firstCutScene, secondCutScene;
+	private long lastCutsceneTime;
+
+	private final int CUTSCENE_DISPLAY_LENGTH = 2000;
 	
 	public Ridout (int dmg, int hp, int speed, int xPos, int yPos, Image image, Dimension size, int maxHP, int numLives,boolean shouldMove, boolean anger)
 	{
 		super (dmg, hp, speed, xPos ,yPos, image, size, maxHP, shouldMove, anger, new Dimension(82, 16),23, 145);
 		this.numLives = numLives;
 		upgraded = false;
+		playCutScene = false;
+		playedFirstCutscene = false;
+		doneCutscene = false;
+
+		try {
+			firstCutScene = ImageIO.read(getClass().getResourceAsStream("/images/ridoutdead1.png"));
+			secondCutScene = ImageIO.read(getClass().getResourceAsStream("/images/ridoutdead2.png"));
+		}
+		catch(IOException ioe)
+		{
+			System.err.println("Error loading ridout cutscenes");
+		}
+	}
+
+	public boolean resumeGame()
+	{
+		return doneCutscene;
 	}
 
 	/**
@@ -24,10 +50,21 @@ public class Ridout extends Enemy
 	public void loseLife ()
 	{
 		numLives --;
-		if (this.lifeRemaining()) {
+		if (!this.lifeRemaining() && !upgraded) {
 			this.secondLifeUpgrade();
 			upgraded = true;
+			playCutScene = true;
+			lastCutsceneTime = System.currentTimeMillis();
 		}
+	}
+
+	/**
+	 * Returns if Ridout has upgraded to his second form or not
+	 * @return if Ridout has upgraded to his second form or not
+     */
+	public boolean hasUpgraded()
+	{
+		return upgraded;
 	}
 
 	/**
@@ -49,7 +86,37 @@ public class Ridout extends Enemy
 		System.out.println("Oh NO! It seems Ridout has started recursively calling massive stat gains");
 		this.setDamage(this.getDamage() * 4);
 		this.setMaxHP(this.getMaxHP() * 4);
-		this.heal(this.getMaxHP());
+		this.setCurrentHP(this.getMaxHP());
 		this.setSpeed(this.getSpeed() * 4);
+	}
+
+	@Override
+	public void draw(Graphics g)
+	{
+		if(!playCutScene)
+		{
+			super.draw(g);
+		}
+		else
+		{
+			//NOTE: i do not want to do this in the draw method but it is the only consitant update an enemy has
+			if(!playedFirstCutscene) {
+				g.drawImage(firstCutScene, 0, 0, null);
+				if(System.currentTimeMillis() - lastCutsceneTime > CUTSCENE_DISPLAY_LENGTH)
+				{
+					playedFirstCutscene = true;
+					lastCutsceneTime = System.currentTimeMillis();
+				}
+			}
+			else
+			{
+				g.drawImage(secondCutScene, 0, 0, null);
+				if(System.currentTimeMillis() - lastCutsceneTime > CUTSCENE_DISPLAY_LENGTH)
+				{
+					doneCutscene = true;
+					playCutScene = false;
+				}
+			}
+		}
 	}
 }
