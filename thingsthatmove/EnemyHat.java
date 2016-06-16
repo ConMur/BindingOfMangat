b1a19e049b8edcba137b36a9c13c27276b6488b7
Matthew Sun <matthewsun3@gmail.com>
2016-06-15 23:30:57 -0400

    hat fix

--- thingsthatmove/EnemyHat.java
+++ thingsthatmove/EnemyHat.java
@@ -12,6 +12,6 @@ public class EnemyHat extends Enemy
 	public EnemyHat () throws IOException
 	{
 		super (new Enemy(1, 50, 50, 400, 400, ImageIO.read(LevelManager.class.getResourceAsStream("/images/enemies/hatfront.png")),
-                new Dimension(52, 16), 50, true, true, new Dimension(42, 13), 4, 28));
+                new Dimension(86, 36), 50, true, true, new Dimension(44, 10), 20, 65));
 	}
 }
